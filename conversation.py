ffd5c148002d7651c8865a674dee05574c8c6cfe
Danlin Lillemark <danlinlillemark@Danlins-MacBook-Pro-2.local>
2018-12-02 12:00:46 -0800

    conversation update

--- conversation.py
+++ conversation.py
@@ -1,5 +1,6 @@
 import numpy as np
 import sklearn
+from parser import *
 
 class Conversation:
 
@@ -9,6 +10,9 @@ class Conversation:
         PyTorch models / word vectors / etc. that you
         have previously trained.
         '''
+        self.notes = []
+        self.lastCommand = False
+    
         pass
 
     def respond(self, sentence):
@@ -18,5 +22,41 @@ class Conversation:
         some input from the user, and return the output
         from the chatbot.
         '''
+        if self.lastCommand:
+            self.lastCommand = False
+            self.takeNote(sentence)
+            return "Note taken."
+
+        func = classify(tokenize(sentence))
+        if func == 0:
+            self.lastCommand = True
+            return "What would you like me to note?"
+        elif func == 1:
+            return self.retrieveNote()
+        elif func == 2:
+            return self.deleteNote()
+        elif func == 3:
+            return self.totalNotes()
+
         return sentence
+
+    def takeNote(self, sentence):
+        self.notes.append(sentence)  
+
+    def retrieveNote(self):
+        if self.notes:
+            return self.notes[-1]
+        else:
+            return "You have no notes."
+    
+    def deleteNote(self):
+        if self.notes:
+            return "Deleted " + self.notes.pop()
+        else:
+            return "You have no notes to delete."
+
+    def totalNotes(self):
+        return "You have " + str(len(self.notes)) + " notes."
+
+    
 
