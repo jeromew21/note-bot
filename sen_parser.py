3e06e4dbf715dd9e6be0dca90882788822a8fec8
Jerome Wei <jeromew@berkeley.edu>
2018-12-02 13:29:01 -0800

    did some things

--- sen_parser.py
+++ sen_parser.py
@@ -9,10 +9,10 @@ def synsets(word):
         yield syn
 
 keywords = [
-    wordnet.synset("take.v.01"),
-    wordnet.synset("retrieve.v.02"),
-    wordnet.synset("delete.v.01"),
-    wordnet.synset('total.v.01'),
+    [wordnet.synset("take.v.01")],
+    [wordnet.synset("retrieve.v.02"), wordnet.synset("get.v.01")],
+    [wordnet.synset("delete.v.01"), wordnet.synset("remove.v.01")],
+    [wordnet.synset('total.v.01'), wordnet.synset("many.a.01")],
 ]
 
 #for vec in keyword_vectors:
@@ -33,17 +33,18 @@ def tokenize(sentence):
 def classify(tokens):
     max_max_word_similarity = -1000
     max_keyword_index = 0
-    for i, synset in enumerate(keywords):
+    for i, synset_list in enumerate(keywords):
         max_word_similarity = -10000
         max_word = None
-        for word in tokens:
-            to_sum = [synset.wup_similarity(ss) for ss in synsets(word)]
-            similarity = sum(to_sum)/len(to_sum) #Mean
-            if similarity > max_word_similarity:
-                max_word_similarity = similarity
-                max_word = word
+        for synset in synset_list:
+            for word in tokens:
+                to_sum = [synset.wup_similarity(ss) for ss in synsets(word)]
+                to_sum = [0 if k is None else k for k in to_sum]
+                similarity = sum(to_sum)/len(to_sum) #Mean
+                if similarity > max_word_similarity:
+                    max_word_similarity = similarity
+                    max_word = word
         if max_word_similarity > max_max_word_similarity:
             max_max_word_similarity = max_word_similarity
             max_keyword_index = i
-        print(synset, max_word, max_word_similarity)
     return max_keyword_index
